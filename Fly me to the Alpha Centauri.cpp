654556ef4e2965c8193a0ce63fbf8a1278a4dd81
chosoom <52771647+chosoom@users.noreply.github.com>
2019-07-26 16:47:56 +0900

    Add files via upload

---
+++ Fly me to the Alpha Centauri.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void fly(long x,long y) {
+	int length = y - x, i, cnt = 1,sw=1 ,cnt2=1;
+	/*for (i = 1; i*2 <= length;) {
+		if (sw>2) {
+			cnt2++; sw = 1;
+		}
+		i += cnt2;
+		if (i > length) break;
+		sw++;
+		cnt++;
+	}*/
+	long sum = 0;
+	for (i = 1; sum <= length; i++) { 
+		cout << sum << endl;
+		sum += i * 2;
+	}
+	i--;
+	cout <<i<<" "<< sum << endl;
+	sum = (i - 1)*i;
+	cout << sum << endl;
+	if (i <= length - sum) cout << i+i-1 << endl;
+	else cout << i+1 +i-1 <<endl;
+}
+int main() {
+	long num, x, y;
+	for (cin >> num; num > 0; num--) {
+		cin >> x >> y;
+		fly(x, y);
+	}
+}
